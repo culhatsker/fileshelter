9925f907cbbb366f384f9cf8ea66efc58e98692c
Krotov Maksim <aloneik@yandex.ru>
2019-06-12 17:26:27 +0300

    Add current folder label

--- fileshelter/app.py
+++ fileshelter/app.py
@@ -8,9 +8,11 @@ files_dir = os.path.abspath("./files")
 if not os.path.isdir(files_dir):
     os.makedirs(files_dir)
 
+
 @app.route("/")
 def root_view():
     return redirect(url_for("files_list_view"))
+
 
 @app.route("/files", defaults={"directory": ""})
 @app.route("/files/", defaults={"directory": ""})
@@ -23,7 +25,7 @@ def files_list_view(directory):
             {
                 "name": name,
                 "directory": os.path.isdir(
-                    os.path.join(files_dir, name))
+                    os.path.join(files_dir, directory, name))
             }
             for name in os.listdir(os.path.join(files_dir, directory))
         ],
@@ -31,12 +33,14 @@ def files_list_view(directory):
         normpath=os.path.normpath
     )
 
+
 @app.route("/internal/download/<path:filepath>")
 def download_view(filepath):
     filepath = os.path.abspath(os.path.join(files_dir, filepath))
     if not filepath.startswith(files_dir):
         raise Exception("Can't access this path")
     return send_file(filepath)
+
 
 @app.route("/internal/upload", defaults={"directory": ""}, methods=["POST"])
 @app.route("/internal/upload/", defaults={"directory": ""}, methods=["POST"])
@@ -54,6 +58,7 @@ def upload_view(directory):
     file.save(saveto)
     return redirect(url_for("files_list_view", directory=directory))
 
+
 @app.route("/internal/create_directory", methods=["POST"])
 def create_directory_view():
     directory = request.form["directory"]
@@ -62,6 +67,7 @@ def create_directory_view():
         raise Exception("Can't access this path")
     os.makedirs(absdir)
     return redirect(url_for("files_list_view", directory=directory))
+
 
 @app.route("/internal/move", methods=["POST"])
 def move_file_view():
