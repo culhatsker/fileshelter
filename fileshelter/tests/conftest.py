44b0e0ddf693daf4fa82f743f8bfd1a929ea71de
Egor Tiuvaev <egor.tyuvaev@gmail.com>
2019-07-02 10:07:08 +0300

    Add tests and a CI pipeline (#11)
    * Add tests and a CI pipeline
    Updated requirements.txt with pytest
    
    * Remove unnecessary comment from conftest.py
    
    * Add coverage metrics, full file_access test coverage
    
    Update .drone.yml to test with coverage, update requirements.txt

---
+++ fileshelter/tests/conftest.py
@@ -0,0 +1,19 @@
+from pytest import fixture
+
+import os
+import shutil
+from uuid import uuid4
+
+@fixture(scope="session")
+def global_tests_dir():
+    dir_path = os.path.abspath("./.test-tmp-{}".format(uuid4()))
+    os.makedirs(dir_path)
+    yield dir_path
+    shutil.rmtree(dir_path)
+
+@fixture
+def test_dir(global_tests_dir):
+    dir_path = os.path.join(global_tests_dir, "test-{}".format(uuid4()))
+    os.makedirs(dir_path)
+    yield dir_path
+    shutil.rmtree(dir_path)
