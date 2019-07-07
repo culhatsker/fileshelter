835e633b2b837ab59fe2b4e07ae2e286810e5b6b
Egor Tyuvaev <egor.tyuvaev@gmail.com>
2019-07-07 17:46:05 +0300

    Remove redundand form fields, simplify code

--- fileshelter/static/file_upload.js
+++ fileshelter/static/file_upload.js
@@ -1,32 +1,18 @@
 (function () {
-  var fileCatcher = document.getElementById('uploadForm');
+  var uploadForm = document.getElementById('uploadForm');
   var fileInput = document.getElementById('uploadFiles');
-
-  var directory = document.getElementById('upload-path')
   
-  var fileList = [];
-  var sendFile;
-  
-  fileCatcher.addEventListener('submit', function (evnt) {
-  	evnt.preventDefault();
+  uploadForm.addEventListener('submit', function (evnt) {
+    evnt.preventDefault();
+    for (let file of fileInput.files) {
+      var formData = new FormData();
+      var request = new XMLHttpRequest();
+      formData.set('file', file);
+      request.open('POST', uploadForm.action);
+      request.send(formData);
+    }
     fileList.forEach(function (file) {
     	sendFile(file);
     });
   });
-  
-  fileInput.addEventListener('change', function (evnt) {
- 		fileList = [];
-  	for (var i = 0; i < fileInput.files.length; i++) {
-    	fileList.push(fileInput.files[i]);
-    }
-  });
-  
-  sendFile = function (file) {
-  	var formData = new FormData();
-    var request = new XMLHttpRequest();
- 
-    formData.set('file', file);
-    request.open("POST", directory);
-    request.send(formData);
-  };
 })();
